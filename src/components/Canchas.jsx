40da28166ca2af7d39ddfc1430b3127bf6757c42
Kevin Serrano <kevinserrano@Kevins-Air.fibertel.com.ar>
2024-08-26 20:48:54 -0300

    Creacion de componente CardCancha y un archivo json de prueba

--- src/components/Canchas.jsx
+++ src/components/Canchas.jsx
@@ -1,8 +1,7 @@
+import { CardCancha } from "./CardCancha"
+import data from '../assets/canchas.json'
 
 export const Canchas = () => {
-    const anchoCard = {
-        width: "auto"
-    }
   return (
     <div className="container">
         <div className="row">
@@ -10,45 +9,19 @@ export const Canchas = () => {
                 <h2>Canchas en Salta</h2>
             </div>
         </div>
-        {/* Comvertir estas cards en un componente */}
         <div className="row">
-            <div className="col-12 col-md-4 mb-5">
-                <div className="card" style={anchoCard}>
-                    <img src="/canchaPrueba.jpg" className="card-img-top" alt="cancha prueba" />
-                    <div className="card-body">
-                        <h5 className="card-title">Titulo de la cancha</h5>
-                        <p className="card-text">Descriccion de la cancha.</p>
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Tipo de cancha por ejemplo F5</li>
-                        <li className="list-group-item">Dias y Horarios</li>
-                        <li className="list-group-item">Precio</li>
-                    </ul>
-                    <div className="card-body">
-                        <a href="#" className="card-link">Ver</a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-4 mb-5">
-                <div className="card" style={anchoCard}>
-                    <img src="/canchaPrueba2.jpg" className="card-img-top" alt="cancha prueba" />
-                    <div className="card-body">
-                        <h5 className="card-title">San Lorenzo</h5>
-                        <p className="card-text">Esta cancha fue habilitada hace mas de 3 años y se llena todos los fines de semana.</p>
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Tipo: Futbol 8</li>
-                        <li className="list-group-item">Dias: Lunes a Sabados</li>
-                        <li className="list-group-item">Horarios: 20:00 a 01:00</li>
-                        <li className="list-group-item">Precio: $10.000</li>
-                    </ul>
-                    <div className="card-body">
-                        <a href="#" className="card-link">Ver</a>
-                    </div>
-                </div>
+                {!data ? 
+                    <p>No hay canchas disponibles</p> 
+                :
+                    data.map((cancha) => {
+                        return (
+                            <div key={cancha.id} className="col-12 col-md-4 mb-4">
+                                <CardCancha cancha={cancha} />
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
-
-    </div>
   )
 }
---
+++ src/components/CardCancha.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+export const CardCancha = ({ cancha }) => {
+  const anchoCard = {
+    width: "auto",
+  };
+  return (
+    <div className="card" style={anchoCard}>
+      <img
+        src= {cancha.imagen}
+        className="card-img-top"
+        alt="cancha prueba"
+      />
+      <div className="card-body">
+        <h5 className="card-title"> {cancha.nombre} </h5>
+        <p className="card-text"> {cancha.descripcion} </p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Ubicacion: {cancha.ubicacion} </li>
+        <li className="list-group-item">Tipo: {cancha.tipo} </li>
+        <li className="list-group-item">
+          Dias disponibles: {cancha.dias_disponibles}{" "}
+        </li>
+        <li className="list-group-item">
+          Horarios: {cancha.horarios_disponibles}{" "}
+        </li>
+        <li className="list-group-item">Precio: {cancha.precio} </li>
+      </ul>
+      <div className="card-body">
+        <a href="#" className="card-link">
+          Ver
+        </a>
+      </div>
+    </div>
+  );
+};

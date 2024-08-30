6cee043a840bf015a2822ffb54af891364c0900b
Kevin Serrano <kevinserrano010@gmail.com>
2024-08-30 17:34:56 -0300

    Listar canchas (listo)

--- src/components/Canchas.jsx
+++ src/components/Canchas.jsx
@@ -1,7 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { CardCancha } from "./CardCancha"
-import data from '../assets/canchas.json'
+import useFetch from "../hooks/useFetchHook"
+import { useAuth } from "../contexts/AuthContext";
+import { useEffect } from "react";
+// import data from '../assets/canchas.json'
 
 export const Canchas = () => {
+    const { token } = useAuth('state');
+    const [ {data, isLoading, errors}, doFetch ] = useFetch(`http://127.0.0.1:8000/api/canchas`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token ${token}`,
+        },
+    });
+
+    useEffect(() => {
+        doFetch();
+    }, []);
+
+    if (isLoading) return <h2>Cargando...</h2>;
+    if (errors) return <h2>Error al cargar canchas.</h2>;
+
   return (
     <div className="container">
         <div className="row">
--- src/components/CardCancha.jsx
+++ src/components/CardCancha.jsx
@@ -15,17 +15,16 @@ export const CardCancha = ({ cancha }) => {
 
   return (
     <div className="card" style={anchoCard}>
-      <img
-        src= {cancha.imagen}
-        className="card-img-top"
-        alt="cancha prueba"
-      />
+      {cancha.imagen ? 
+        <img src= {cancha.imagen} className="card-img-top" alt="imagen de cancha"/>
+        : 
+        <img src="./canchaPrueba2.jpg" className="card-img-top" alt="cancha prueba"/>
+      }
       <div className="card-body">
         <h5 className="card-title"> {cancha.nombre} </h5>
-        <p className="card-text"> {cancha.descripcion} </p>
       </div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">Ubicacion: {cancha.ubicacion} </li>
+        <li className="list-group-item">Ubicacion: {cancha.direccion} </li>
         <li className="list-group-item">Tipo: {cancha.tipo} </li>
       </ul>
       <div className="card-body">

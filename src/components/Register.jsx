1e66f833172589feabab173c392c35eeb8f5e2e4
Kevin Serrano <kevinserrano010@gmail.com>
2024-09-04 11:40:47 -0300

    Registrar un usuario (Listo)

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,52 +1,181 @@
+import { useNavigate } from "react-router-dom";
+import useFetch from "../hooks/useFetchHook";
+import { useState } from "react";
 
 export const Register = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    first_name: "",
+    last_name: "",
+    celular: "",
+    password: "",
+    imagen: null,
+  });
 
-    // funcion para registart usuario y redirigir a la pagina de login
-    const handleSubmit = (event) => {
-        // Implementar el registro
-        event.preventDefault();
-        //recibir los datos y mostrarlos en consola
-        console.log(event.target.fullname.value);
-        console.log(event.target.email.value);
-        console.log(event.target.celular.value);
-        console.log(event.target.password.value);
-        
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    setFormData({
+      ...formData,
+      imagen: e.target.files[0], // Solo se permite seleccionar un archivo
+    });
+  };
+
+  const [{ data, isError, isLoading }, doFetch] = useFetch(
+    "http://127.0.0.1:8000/api/register/",
+    {
+      method: "POST",
     }
+  );
+
+  // funcion para registart usuario y redirigir a la pagina de login
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Crea un objeto FormData para enviar datos al backend
+    const form = new FormData();
+    form.append("username", formData.username);
+    form.append("email", formData.email);
+    form.append("first_name", formData.first_name);
+    form.append("last_name", formData.last_name);
+    form.append("celular", formData.celular);
+    form.append("password", formData.password);
+    form.append("imagen", formData.imagen); // Incluye la imagen en el form
+
+    doFetch({ body: form });
+    alert("Usuario registrado con exito");
+    navigate("/login");
+  };
 
   return (
     <div className="container">
-        <div className="row">
-            <div className="col-4"></div>
+      <div className="row">
+        <div className="col-4"></div>
 
-            <div className="col-4">
-                <h2 className="text-center mb-5">Register</h2>
-                <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                        <label htmlFor="fullname" className="form-label">Nombre Completo</label>
-                        <input type="text" className="form-control" id="fullname" name="fullname" aria-describedby="emailHelp" required/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Correo electronico</label>
-                        <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" required/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="celular" className="form-label">Celular</label>
-                        <input type="number" className="form-control" id="celular" name="celular" aria-describedby="emailHelp" required/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="password" name="password" required/>
-                    </div>
-                    <div className="mb-3 text-center">
-                        <div className="control">
-                            <button type="submit" className="btn btn-primary text-center">Registrarme</button>
-                        </div>
-                    </div>
-                </form>
+        <div className="col-4">
+          <h2 className="text-center mb-5">Register</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="first_name" className="form-label">
+                Nombre
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="first_name"
+                name="first_name"
+                value={formData.first_name}
+                onChange={handleChange}
+                required
+              />
             </div>
-
-            <div className="col-4"></div>
+            <div className="mb-3">
+              <label htmlFor="last_name" className="form-label">
+                Apellido
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="last_name"
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Nombre de usuario
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Correo electronico
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="celular" className="form-label">
+                Celular
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="celular"
+                name="celular"
+                value={formData.celular}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="imagen" className="form-label">
+                foto de perfil
+              </label>
+              <div className="control has-icons-left">
+                <input
+                  className="form-control"
+                  type="file"
+                  id="imagen"
+                  name="imagen"
+                  onChange={handleFileChange}
+                />
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="mb-3 text-center">
+              <div className="control">
+                <button type="submit" className="btn btn-primary text-center">
+                  Registrarme
+                </button>
+              </div>
+            </div>
+            <div className="mb-3 text-center">
+              <div className="control">
+                {isLoading && <p>Cargando...</p>}
+                {isError && <p>Error al cargar los datos.</p>}
+                {data && <p>Usuario registrado con exito</p>}
+              </div>
+            </div>
+          </form>
         </div>
+
+        <div className="col-4"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
